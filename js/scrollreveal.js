a8cb86a800271d76a2adf15baf3777327cbf144d
Diego Grassino <dg27ar@MacBook-Pro-de-Diego.local>
2022-01-20 18:21:20 -0300

    Arreglo validaciones de form, creo archivos js aparte para features

---
+++ js/scrollreveal.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+  duration: 2000,
+  origin: 'top',
+});
+sr.reveal('.header-title', {
+  duration: 2000,
+  origin: 'rigth',
+  distance: '300px',
+});
+sr.reveal('.header-subtitle', {
+  duration: 2000,
+  origin: 'left',
+  distance: '300px',
+});
+sr.reveal('.header-button', {
+  duration: 2000,
+  origin: 'bottom',
+  delay: 750,
+});
+sr.reveal('.about-me-content', {
+  duration: 2000,
+  origin: 'right',
+  distance: '300px',
+});
